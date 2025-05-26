69e5335746841bd7b97095e85e061331bf7b8bfd
Davi Brasil <davi.brasil2005@gmail.com>
2025-05-26 17:12:14 -0400

    estado-criar-tarefas

--- vite-project/src/components/formCriarTarefa/FormCriarTarefa.jsx
+++ vite-project/src/components/formCriarTarefa/FormCriarTarefa.jsx
@@ -1,20 +1,34 @@
-import {CampoTexto, Botao} from '../campotexto'
-
+import { CampoTexto } from '../campotexto'
+import { Botao } from '../botao'
 import style from './FormCriarTarefa.module.css'
 import { useState } from 'react'
 const FormCriarTarefa = (props) =>{
-    const {nomeTarefa, setNomeTarefa} = useState("valorPadrao")
+    const [nomeTarefa, setNomeTarefa] = useState("valorPadrao")
     const{setTarefas} = props;
-    const adicionarTarefa = () => {
-        return(
-            <div>
-
-            </div>
-        )
+    const onChangeNomeTarefa = (event) =>{
+        setNomeTarefa(event.currentTarget.value)
     }
+    const adicionarTarefa = (event) => {
+        event.preventDefault()
+        
+        if(!nomeTarefa){
+            return
+        }
+        setTarefas(estadoAtual => {
+            const tarefa = {
+                id: estadoAtual.length + 1,
+                nome: nomeTarefa,
+            }
+            return[
+                ...estadoAtual,
+                tarefa,
+            ]
+        })
+        setNomeTarefa('')
+     }
     return(
-        <form className = {style.FormCriarTarefa}>
-            <CampoTexto value = {nomeTarefa}/>
+        <form className = {style.FormCriarTarefa} onSubmit = {adicionarTarefa}>
+            <CampoTexto value = {nomeTarefa} onChange ={onChangeNomeTarefa}/>
             <Botao texto ="+"/>
         </form>
     )
--- vite-project/src/pages/inicial/Inicial.jsx
+++ vite-project/src/pages/inicial/Inicial.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { FormCriarTarefa, ListaTarefas} from "../../components"
 import style from "./Inicial.module.css"
 const Inicial = () => {
-    const{tarefas,setTarefas} = useState([
+    const[tarefas,setTarefas] = useState([
         {id: 1, nome: "item 1"},
         {id: 2, nome: "item 2"},
         {id: 3, nome: "item 3"},
